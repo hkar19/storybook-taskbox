2b81d260d9edd31acf639e1bd558b67db346ed24
Hadi Kurniawan AR <hadikurniawanar@gmail.com>
2021-06-20 04:50:57 +0700

    - simple component

---
+++ src/components/Task.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Task = ({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) => {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => (onArchiveTask ? onArchiveTask(id) : undefined)}
+        />
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly placeholder="Input title" />
+      </div>
+
+      <div className="actions" onClick={(e) => e.stopPropagation()}>
+        {state !== "TASK_ARCHIVED" && (
+          <a onClick={() => (onPinTask ? onPinTask(id) : undefined)}>
+            <span className={`icon-start`} />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    /** id of the task */
+    id: PropTypes.string.isRequired,
+    /** title of the task */
+    title: PropTypes.string.isRequired,
+    /** current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
+};
